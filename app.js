51576633ad83c8b5bf963243d0ea37b16bdceee0
Franko Stipanov <fstipanov@gmail.com>
2021-03-10 11:51:23 +0100

    add js file

---
+++ app.js
@@ -0,0 +1,20 @@
+/*************************************************
+ *** digital clock
+ */
+
+const domStrings = {
+    h: '.hours',
+    m: '.minutes',
+    s: '.seconds',
+}
+
+const time = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    document.querySelector(domStrings.h).innerHTML = hours;
+    document.querySelector(domStrings.m).innerHTML = minutes;
+    document.querySelector(domStrings.s).innerHTML = seconds;
+};
+setInterval(time, 1000);
