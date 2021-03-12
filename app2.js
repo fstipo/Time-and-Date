a6e2c454b9e3a05b9993020bba1a8bff5e8f69e6
Franko Stipanov <fstipanov@gmail.com>
2021-03-12 16:30:55 +0100

    add countdown - working

--- app2.js
+++ app2.js
@@ -8,26 +8,29 @@ const domStrings = {
     s: '.seconds',
     today: '.today',
     endDate: '#end_date',
-    cd: 'cnt_days',
-    ch: 'cnt_hours',
-    cm: 'cnt_minutes',
-    cs: 'cnt_seconds',
+    cd: '.cnt_days',
+    ch: '.cnt_hours',
+    cm: '.cnt_minutes',
+    cs: '.cnt_seconds',
     dateSelected: '.output_date'
 };
+let timeInterval;
+let timeStop = true;
 
 
 // change date
 
 const changeDate = document.querySelector(domStrings.endDate);
 changeDate.addEventListener('change', (e) => {
+    clearInterval(timeInterval);
     temp = changeDate.value
     temp = new Date(temp);
-    console.log(temp);
     startClock(temp);
+    timeStop = false;
 });
 
 
-// funcion format date
+// function format date
 const formatDate = (d) => {
     const year = d.getFullYear();
     const monthIndex = d.getMonth();
@@ -38,39 +41,44 @@ const formatDate = (d) => {
     const day = days[dayIndex];
     const date = d.getDate();
     return (`${day}, ${date} ${month} ${year}`)
-}
-
-
-
-
+};
 
 startClock = (d) => {
-    timeLeft(d);
+    let updateCounter = () => {
+        let tl = timeLeft(d);
+        if (tl.total <= 0) {
+            timeStop = false;
+        };
+
+        document.querySelector(domStrings.cd).innerHTML = tl.days;
+        document.querySelector(domStrings.ch).innerHTML = tl.hours;
+        document.querySelector(domStrings.cm).innerHTML = tl.minutes;
+        document.querySelector(domStrings.cs).innerHTML = tl.seconds;
+    }
+    updateCounter();
+    if (timeStop) {
+        timeInterval = setInterval(updateCounter, 1000);
+    } else {
+        clearInterval(timeInterval);
+    }
 }
 
 timeLeft = (d) => {
     endDate = formatDate(d);
     const now = new Date();
     const today = formatDate(now);
-    console.log(today)
     document.querySelector(domStrings.today).innerHTML = today;
-    console.log(Date.parse(now));
     document.querySelector(domStrings.dateSelected).innerHTML = endDate;
-    console.log(Date.parse(endDate));
+    totalTime = Date.parse(endDate) - Date.parse(now);
+    let seconds = Math.floor((totalTime / 1000) % 60);
+    let minutes = Math.floor((totalTime / 1000 / 60) % 60);
+    let hours = Math.floor(totalTime / (1000 * 60 * 60) % 24);
+    let days = Math.floor(totalTime / (1000 * 60 * 60 * 24));
+    return {
+        'total': seconds,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
 }
-
-// // formatSelectDate = formatDate(selectedDate);
-// document.querySelector(domStrings.dateSelected).innerHTML = formatSelectDate;
-// console.log(selectedDate);
-// return selectedDate;
-
-
-
-
-// document.querySelector(domStrings.dateSelected).innerHTML = endDay;
-
-
-
-// const timeLeft = (endDate, now) => {
-//     return Date.parse(endDate) - Date.parse(now);
-// };
